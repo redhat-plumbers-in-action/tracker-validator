7a0827845730678c8a3bf39f36eae45c7026d693
Jan Macku <jamacku@redhat.com>
2024-03-26 16:26:59 +0100

    refactor: use ref `from` metadata and repo/owner from context

---
+++ dist/action.js
@@ -0,0 +1,104 @@
+import { debug, error, getInput, notice } from '@actions/core';
+import { context } from '@actions/github';
+import { z } from 'zod';
+import { Bugzilla } from './bugzilla';
+import { Config } from './config';
+import { Controller } from './controller';
+import { Jira } from './jira';
+import { getFailedMessage, getSuccessMessage, raise, removeLabel, setLabels, setTitle, } from './util';
+async function action(octokit, prMetadata) {
+    const trackerType = getInput('tracker-type', { required: true });
+    const config = await Config.getConfig(octokit);
+    let trackerController;
+    switch (trackerType) {
+        case 'bugzilla':
+            const bzInstance = getInput('bugzilla-instance', { required: true });
+            const bzAPIToken = getInput('bugzilla-api-token', { required: true });
+            trackerController = new Controller(new Bugzilla(bzInstance, bzAPIToken));
+            debug(`Using Bugzilla '${bzInstance}', version: '${await trackerController.adapter.getVersion()}'`);
+            break;
+        case 'jira':
+            const jiraInstance = getInput('jira-instance', { required: true });
+            const jiraAPIToken = getInput('jira-api-token', { required: true });
+            trackerController = new Controller(new Jira(jiraInstance, jiraAPIToken));
+            debug(`Using Jira '${jiraInstance}', version: '${await trackerController.adapter.getVersion()}'`);
+            break;
+        default:
+            setLabels(octokit, prMetadata.number, [config.labels['missing-tracker']]);
+            raise(`🔴 Missing tracker or Unknown tracker type; type: '${trackerType}'`);
+    }
+    let message = [];
+    let err = [];
+    let labels = { add: [], remove: [] };
+    const labelsFromPR = z
+        .array(z.object({ name: z.string() }).transform(label => label.name))
+        .parse((await octokit.request('GET /repos/{owner}/{repo}/issues/{issue_number}/labels', Object.assign(Object.assign({}, context.repo), { issue_number: prMetadata.number }))).data);
+    const tracker = getInput('tracker', { required: true });
+    let issueDetails;
+    try {
+        issueDetails = await trackerController.adapter.getIssueDetails(tracker);
+        if (labelsFromPR.includes(config.labels['missing-tracker'])) {
+            removeLabel(octokit, prMetadata.number, config.labels['missing-tracker']);
+        }
+    }
+    catch (e) {
+        setLabels(octokit, prMetadata.number, [config.labels['missing-tracker']]);
+        error(`getIssueDetails(${tracker}): ${e}`);
+        raise(`🔴 Tracker '${tracker}' does not exist on ${trackerController.adapter.instance}`);
+    }
+    const titleResult = await setTitle(octokit, prMetadata.number, tracker, trackerType);
+    notice(`🔤 ${titleResult}`);
+    const isMatchingProduct = trackerController.adapter.isMatchingProduct(config.products);
+    if (!isMatchingProduct) {
+        labels.add.push(config.labels['invalid-product']);
+        err.push(`🔴 Tracker ${trackerController.adapter.getMarkdownUrl()} has product \`${issueDetails.product}\` but desired product is one of \`${config.products}\``);
+    }
+    else {
+        if (labelsFromPR.includes(config.labels['invalid-product'])) {
+            removeLabel(octokit, prMetadata.number, config.labels['invalid-product']);
+        }
+        // Set base branch as label if it is not main or master (rhel-9.0.0, rhel-8.5.0, rhel-7.9, etc.)
+        if (prMetadata.base != 'main' &&
+            prMetadata.base != 'master' &&
+            !labelsFromPR.includes(prMetadata.base)) {
+            labels.add.push(prMetadata.base);
+        }
+        message.push(`🟢 Tracker ${trackerController.adapter.getMarkdownUrl()} has set desired product: \`${issueDetails.product}\``);
+    }
+    const component = getInput('component', { required: true });
+    const isMatchingComponent = trackerController.adapter.isMatchingComponent(component);
+    if (!isMatchingComponent) {
+        labels.add.push(config.labels['invalid-component']);
+        err.push(`🔴 Tracker ${trackerController.adapter.getMarkdownUrl()} has component \`${issueDetails.component}\` but desired component is \`${component}\``);
+    }
+    else {
+        if (labelsFromPR.includes(config.labels['invalid-component'])) {
+            removeLabel(octokit, prMetadata.number, config.labels['invalid-component']);
+        }
+        message.push(`🟢 Tracker ${trackerController.adapter.getMarkdownUrl()} has set desired component: \`${component}\``);
+    }
+    if (!trackerController.adapter.isApproved()) {
+        labels.add.push(config.labels.unapproved);
+        err.push(`🔴 Tracker ${trackerController.adapter.getMarkdownUrl()} has not been approved`);
+    }
+    else {
+        if (labelsFromPR.includes(config.labels.unapproved)) {
+            removeLabel(octokit, prMetadata.number, config.labels.unapproved);
+        }
+        message.push(`🟢 Tracker ${trackerController.adapter.getMarkdownUrl()} has been approved`);
+    }
+    if (isMatchingProduct && isMatchingComponent) {
+        const linkMessage = await trackerController.adapter.addLink('https://github.com/', `${context.repo.owner}/${context.repo.repo}/pull/${prMetadata.number}`);
+        notice(`🔗 ${linkMessage}`);
+        const stateMessage = await trackerController.adapter.changeState();
+        notice(`🎺 ${stateMessage}`);
+    }
+    // TODO: Once validated update Tracker status and add/update comment in PR
+    setLabels(octokit, prMetadata.number, labels.add);
+    if (err.length > 0) {
+        raise(getFailedMessage(err) + '\n\n' + getSuccessMessage(message));
+    }
+    return getSuccessMessage(message);
+}
+export default action;
+//# sourceMappingURL=action.js.map
--- dist/util.d.ts
+++ dist/util.d.ts
@@ -1,11 +1,11 @@
 import { Octokit } from '@octokit/core';
-export declare function updateStatusCheck(octokit: Octokit, checkID: number, owner: string, repo: string, status: undefined, conclusion: 'action_required' | 'cancelled' | 'failure' | 'neutral' | 'success' | 'skipped' | 'stale' | 'timed_out' | undefined, message: string): Promise<void>;
+export declare function updateStatusCheck(octokit: Octokit, checkID: number, status: undefined, conclusion: 'action_required' | 'cancelled' | 'failure' | 'neutral' | 'success' | 'skipped' | 'stale' | 'timed_out' | undefined, message: string): Promise<void>;
 export declare function getFailedMessage(error: string[]): string;
 export declare function getSuccessMessage(message: string[]): string;
-export declare function setLabels(octokit: Octokit, owner: string, repo: string, issueNumber: number, labels: string[]): Promise<void>;
-export declare function removeLabel(octokit: Octokit, owner: string, repo: string, issueNumber: number, label: string): Promise<void>;
+export declare function setLabels(octokit: Octokit, issueNumber: number, labels: string[]): Promise<void>;
+export declare function removeLabel(octokit: Octokit, issueNumber: number, label: string): Promise<void>;
 export declare function raise(error: string): never;
-export declare function getTitle(octokit: Octokit, owner: string, repo: string, issueNumber: number): Promise<string>;
+export declare function getTitle(octokit: Octokit, issueNumber: number): Promise<string>;
 export declare function isTrackerInTitle(title: string, tracker: string): boolean;
 export declare function getCurrentTitle(title: string): string;
-export declare function setTitle(octokit: Octokit, owner: string, repo: string, issueNumber: number, tracker: string, trackerType: 'bugzilla' | 'jira'): Promise<string>;
+export declare function setTitle(octokit: Octokit, issueNumber: number, tracker: string, trackerType: 'bugzilla' | 'jira'): Promise<string>;
