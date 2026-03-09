979b7a8a2718d6f2c2b30cc4294e8d5552411f30
Jan Macku <jamacku@redhat.com>
2026-03-09 13:22:21 +0100

    build: migrate from ncc to esbuild

--- dist/action.d.ts
+++
@@ -1,4 +0,0 @@
-import { CustomOctokit } from './octokit';
-import { PullRequestMetadata } from './schema/input';
-declare function action(octokit: CustomOctokit, prMetadata: PullRequestMetadata): Promise<string>;
-export default action;
