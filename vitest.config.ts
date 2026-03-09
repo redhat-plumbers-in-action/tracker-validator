4c023869de1c77ea65d17e0b205ecae371cc0b67
Jan Macku <jamacku@redhat.com>
2026-03-09 13:15:35 +0100

    deps: update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    dir: './test',
+    exclude: [...configDefaults.exclude],
     // Make sure we load the dotenv config before running tests
     setupFiles: ['dotenv/config'],
     // API calls can take a while, so we need to increase the timeout
     testTimeout: 15000,
+    coverage: {
+      include: ['src/**/*.ts'],
+    },
   },
 });
